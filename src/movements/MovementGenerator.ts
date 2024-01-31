99b441f3dae1aa0da4a0d59332b4f9167543a5ed
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-01-31 17:00:04 +0100

    Update the MovementGenerator to an abstract class

--- src/movements/MovementGenerator.ts
+++ src/movements/MovementGenerator.ts
@@ -1,4 +1,17 @@
-interface MovementGenerator{
-    getType() : string;
-    generateAvaibleMoves(currentPos : number[]) : number[][];
+abstract class MovementGenerator{
+    protected MAX_BOARD_SIZE : number = 8;
+    protected MIN_BOARD_SIZE : number = 0;
+    protected MOVEMENT_TYPE : string;
+
+    constructor(movementType : string){
+        this.MOVEMENT_TYPE = movementType; 
+    }
+
+    getType(): string {
+        return this.MOVEMENT_TYPE;
+    }
+
+    abstract generateAvaibleMoves(currentPos : number[]) : number[][];
 }
+
+export default MovementGenerator;
