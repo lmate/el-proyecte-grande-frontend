7cc0344582a8ef762f87b0ba3c17a514823148bf
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-01 12:32:36 +0100

    Filter out the wrong moves for the king

--- src/movements/KingMovement.ts
+++ src/movements/KingMovement.ts
@@ -6,34 +6,49 @@ class KingMovement extends MovementGenerator{
     }
 
     generateAvaibleMoves(currentPos : number[]) : number[][]{
-        return this.getAllMoves(currentPos);
+        const avaibleMoves: number[][] = this.getAllMoves(currentPos);
+        const validMoves: number[][] = this.filterValidMovements(avaibleMoves);
+        return validMoves;
     }
 
     private getAllMoves(currentPos: number[]) : number[][]{
         const xPos: number = currentPos[0];
-        const yPos: number = currentPos[0];
+        const yPos: number = currentPos[1];
 
-        const forward: number[] = [xPos, yPos + 1];
-        const forwardRight: number[] = [xPos + 1, yPos + 1];
+        const forward: number[] = [xPos, yPos - 1];
+        const forwardRight: number[] = [xPos + 1, yPos - 1];
         const right: number[] = [xPos + 1, yPos];
-        const backwardRight: number[] = [xPos + 1, yPos - 1];
-        const backward: number[] = [xPos, yPos - 1];
-        const backwardLeft: number[] = [xPos - 1, yPos - 1];
+        const backwardRight: number[] = [xPos + 1, yPos + 1];
+        const backward: number[] = [xPos, yPos + 1];
+        const backwardLeft: number[] = [xPos - 1, yPos + 1];
         const left: number[] = [xPos - 1, yPos];
-        const forwardLeft: number[] = [xPos - 1, yPos + 1];
+        const forwardLeft: number[] = [xPos - 1, yPos - 1];
 
         const allMoves: number[][] = [
-            backward,
-            backwardLeft,
-            left,
-            forwardLeft,
             forward,
             forwardRight,
             right,
-            backwardRight
+            backwardRight,
+            backward,
+            backwardLeft,
+            left,
+            forwardLeft
         ];
 
         return allMoves;
+    }
+
+    private filterValidMovements(avaibleMoves: number[][]){
+        const filteredMoves: number[][] = [];
+
+        for(const move of avaibleMoves){
+            if(!(move.includes(-1) || move.includes(8))){
+                filteredMoves.push(move);
+            }
+        }
+    
+
+        return filteredMoves;
     }
 }
 
