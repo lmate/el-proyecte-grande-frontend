3a47332add861c9298093c0723ed62bfaa2aa368
Mate Lenkey <9825717+lmate@users.noreply.github.com>
2024-02-14 00:39:54 +0100

    Add new router in App.jsx to make state sharing easier

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
-import React from 'react'
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
