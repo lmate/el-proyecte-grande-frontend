d49f17db01d2e223e1ae10a81fe5f70f910ce846
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-15 10:15:34 +0100

    Create state for max and min scores

--- src/components/Rush.tsx
+++ src/components/Rush.tsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 
 function Rush({disableCick}){
     const [timer, setTimer] = useState<number>(10);
+    const [correctCount, setCorrectCount] = useState<number>(0);
+    const [invalidCount, setInvalidCount] = useState<number>(0);
+
+    const [currentDifficultyMin, setCurrentDifficultyMin] = useState<number>(400);
+    const [currentDifficultyMax, setCurrentDificultyMax] = useState<number>(450);
+    const difficultyIncrementor = 50
 
     useEffect(() => {
         function countDown(){
